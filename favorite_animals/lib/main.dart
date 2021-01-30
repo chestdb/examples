571ebfd67a474805f129b39fda189d5d18d76fdc
Marcel Garus <marcel.garus@gmail.com>
2021-01-31 00:40:51 +0100

    Refactor into tabbed screen

--- favorite_animals/lib/main.dart
+++ favorite_animals/lib/main.dart
@@ -2,8 +2,7 @@ import 'package:animal_names/animal_names.dart';
 import 'package:flutter/material.dart';
 import 'package:chest_flutter/chest_flutter.dart';
 
-const allAnimals = all_animals;
-final favorites = Chest<Set<String>>('favoriteAnimals', ifNew: () => {});
+final favorites = Chest<Set<String>>('favorites', ifNew: () => {});
 
 void main() async {
   await initializeChest();
@@ -18,33 +17,68 @@ void main() async {
 class FavoriteAnimalsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Favorite Animals',
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Favorite Animals')),
-        body: ListView.builder(
-          itemCount: allAnimals.length,
-          itemBuilder: (context, index) {
-            final animal = allAnimals[index];
-            return ListTile(
-              title: Text(animal),
-              trailing: ReferenceBuilder(
-                reference: favorites[animal],
+    return DefaultTabController(
+      length: 2,
+      child: MaterialApp(
+        title: 'Favorite Animals',
+        theme: ThemeData(primarySwatch: Colors.indigo),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Favorite Animals'),
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'All'),
+                Tab(text: 'Favorites'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              ListView.builder(
+                itemCount: all_animals.length,
+                itemBuilder: (context, index) =>
+                    AnimalTile(animal: all_animals[index]),
+              ),
+              ReferenceBuilder(
+                reference: favorites,
                 builder: (_) {
-                  return IconButton(
-                    icon: Icon(
-                      favorites.contains(animal)
-                          ? Icons.favorite
-                          : Icons.favorite_border,
-                    ),
-                    onPressed: () => favorites.toggle(animal),
+                  return ListView(
+                    children: [
+                      for (final animal in favorites.value.toList()..sort())
+                        AnimalTile(animal: animal),
+                    ],
                   );
                 },
               ),
-            );
-          },
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class AnimalTile extends StatelessWidget {
+  const AnimalTile({Key key, @required this.animal}) : super(key: key);
+
+  final String animal;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(animal),
+      trailing: ReferenceBuilder(
+        reference: favorites[animal],
+        builder: (_) {
+          return IconButton(
+            icon: Icon(
+              favorites.contains(animal)
+                  ? Icons.favorite
+                  : Icons.favorite_border,
+            ),
+            onPressed: () => favorites.toggle(animal),
+          );
+        },
       ),
     );
   }

44248c918b798836348fccd8ad998035cad32f4a
Marcel Garus <marcel.garus@gmail.com>
2021-01-30 23:19:19 +0100

    Add dark mode example

---
+++ dark_mode/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:chest_flutter/chest_flutter.dart';
+
+final isDark = Chest<bool>('isDark', ifNew: () => false);
+
+void main() async {
+  await initializeChest();
+  tape.register({
+    ...tapers.forDartCore,
+  });
+  await isDark.open();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ReferenceBuilder(
+      reference: isDark,
+      builder: (_) {
+        return MaterialApp(
+          themeMode: isDark.value ? ThemeMode.dark : ThemeMode.light,
+          darkTheme: ThemeData.dark(),
+          home: Scaffold(
+            body: Center(
+              child: Switch(
+                value: isDark.value,
+                onChanged: (_) => isDark.toggle(),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
